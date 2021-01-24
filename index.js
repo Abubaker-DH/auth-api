5ee1afbf9ae5db980939cfd63f5e572fa32f176b
A Dahab <abubakerdahab@gmail.com>
2021-01-24 11:08:43 +0200

    create server & connect to MongoDB & use config.env to store enviroment varuable

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const app = express();
 
+// connect to Database
+require("./startup/db")();
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`listening on port ${PORT} ...`)
+);
+
+module.exports = server;
